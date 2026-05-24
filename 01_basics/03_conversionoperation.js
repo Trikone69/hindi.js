5da969a50d7141f8045ba4a3a71c8cd44a903b46
Trikone69 <krishnakantm065@gmail.com>
2026-05-24 21:20:52 +0530

    conversion of data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score = "100"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueINnumber = Number(score)
+//console.log(typeof valueINnumber);
+//console.log(valueINnumber);
+
+//"100" => 100
+//"100abc" => NaN
+//"abc100" => NaN
+//true => 1
+//false => 0
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//"" => false
+//" " => true
+//0 => false
+//1 => true
+//-1 => true
+
+let someNumber = 100
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
